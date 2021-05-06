36354bcfa35f2dd0446af0d83227448f5c582d63
thomasvergne <thomas@quark-lang.dev>
2021-05-06 12:31:04 +0200

    🚸 Improved accessibility by adding CLI.

--- Main.hs
+++ Main.hs
@@ -1,10 +1,19 @@
 module Main where
   import Core.Parser
-  import Text.Parsec.Error
-  import Control.Monad.Except
-  import System.Directory
+  import Core.Interpreter
+
   import Types.Element
-  import Core.Build
+  import Types.Stack
+
+  import Text.Parsec.Error
+  import Data.List
+  import System.Directory
+  import System.Environment
+
+  import Control.Monad.Except
+  import Control.Monad.State
+  import Data.Functor.Identity
+
 
   readFile' :: FilePath -> ExceptT String IO String
   readFile' path = do
@@ -24,9 +33,30 @@ module Main where
       Left e -> throwError (show e)
       Right x -> return x
 
+  -- For Brainfuck loops :
+  -- Execute loop content while current cell value is greater than 0
+
+  processArgs :: [String] -> [(String, String)]
+  processArgs [] = []
+  processArgs (x:y:xs) = if (isPrefixOf "-" x) then (x, y) : processArgs xs else processArgs xs
+  processArgs (x:xs) = if (not . isPrefixOf "-" $ x) then [] else processArgs xs
+  
+  getArg :: String -> [(String, String)] -> Maybe (String, String)
+  getArg x = find ((==x) . fst)
+  
+  getNewStack :: Maybe (String, String) -> Stack
+  getNewStack Nothing = initStack 255
+  getNewStack (Just (_, s)) = do
+    let size = read s :: Int
+    initStack (size - 1)
+  
   main :: IO ()
   main = do
-    res <- runExceptT (parseFile "tests/Hello.bf")
-    case res of
+    args <- getArgs
+    let processed = processArgs (drop 1 args)
+    parsed <- runExceptT (parseFile (args !! 0))
+    case parsed of
       Left e -> fail e
-      Right x -> putStrLn (buildFromTree x)
+      Right x -> do
+        _ <- runStateT (eval x) (getNewStack (getArg "-s" processed), 0)
+        return ()
