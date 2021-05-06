c643a9894e0139ce4fc7939343231b577c362854
thomasvergne <thomas@quark-lang.dev>
2021-05-06 12:30:15 +0200

    ✨ Input, output, next, previous and loop evaluation added.

--- core/Interpreter.hs
+++ core/Interpreter.hs
@@ -6,5 +6,13 @@ module Core.Interpreter where
   import Types.Stack
 
   eval :: Program -> StateStack IO ()
+  eval [] = return ()
   eval (Increment:xs) = updateAddressValue Increment >> eval xs
-  eval (Output:xs) = output
+  eval (Decrement:xs) = updateAddressValue Decrement >> eval xs
+  eval (Input:xs) = input >> eval xs
+  eval (Output:xs) = output >> eval xs
+  eval (Next:xs) = movePointer Next >> eval xs
+  eval (Previous:xs) = movePointer Previous >> eval xs
+  eval z@(Loop a:xs) = do
+    cell <- getCurrentCell
+    if cell == 0 then eval xs else eval a >> eval z
